3cf1b59caf3f14ea78cf516eefcc6e130ba20ebb
Saroj_Dev <sarojtech0005@gmail.com>
2025-09-11 11:30:02 +0530

    Added cloudinary config & prod schema

---
+++ src/schema/productSchema.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, "Product title is required"],
+        minLength: [7, "The title of product must be atleast 7 characters long"],
+        minLength: [21, "The title of product must be atmost 21 characters long"],
+    },
+
+    description: {
+        type: String,
+        trim: true,
+        required: [true, "Product title is required"],
+        minLength: [14, "The title of product must be atleast 7 characters long"],
+        minLength: [42, "The title of product must be atmost 21 characters long"],
+    },
+
+    category: {
+        type: String,
+        enum: ["Veg, Non-Veg", "Drinks", "Sides"],
+        default: "Veg"
+    },
+
+    image: {
+        type: String,
+    },
+
+    price: {
+       type: Number,
+       required: [true, "Product price is required"],
+    },
+
+    inStock: {
+        type: Boolean,
+        required: [true, "In stock status is required"]
+    }
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
