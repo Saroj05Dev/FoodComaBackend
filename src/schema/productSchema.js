8395c74a1b0c81dc2077a10a4fb170a20613955a
Saroj_Dev <sarojtech0005@gmail.com>
2025-09-20 14:55:32 +0530

    made changes in prod schema

--- src/schema/productSchema.js
+++ src/schema/productSchema.js
@@ -5,16 +5,12 @@ const productSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: [true, "Product title is required"],
-        minLength: [7, "The title of product must be atleast 7 characters long"],
-        maxLength: [21, "The title of product must be atmost 21 characters long"],
     },
 
     description: {
         type: String,
         trim: true,
         required: [true, "Product title is required"],
-        minLength: [14, "The title of product must be atleast 14 characters long"],
-        maxLength: [42, "The title of product must be atmost 42 characters long"],
     },
 
     category: {
